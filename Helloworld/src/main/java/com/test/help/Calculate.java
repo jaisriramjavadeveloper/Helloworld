2af3340764ae1524c2785d984cb6a6f5dc41dbf1
jaisriramjavadeveloper <sriram.aruna75@gmail.com>
2024-09-29 00:49:41 +0530

    adding helper class

---
+++ Helloworld/src/main/java/com/test/help/Calculate.java
@@ -0,0 +1,11 @@
+package com.test.help;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to java");
+
+	}
+
+}
